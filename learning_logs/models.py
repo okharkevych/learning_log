75fe0479463b1593f6416de540c57154c54abf94
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-03 20:53:09 +0300

    - created a superuser; - added Entry model to models.py to allow users to make entries related to a specific topic; - performed migration for the Entry model; - registered the Topic and Entry models in admin interface via admin.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,17 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
+
+
+class Entry(models.Model):
+    """Some specific information related to this topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns model representation in a string"""
+        return f'{self.text[:50]}...'
