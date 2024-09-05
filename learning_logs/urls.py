cbcacaac80d7c2d3da190cc3a56ba3eeea177f5c
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-05 15:10:11 +0300

    implemented adding new topics from user input form via 'homepage/new_topic' web page that can be accessed directly or through 'Add a new topic' button at the 'topics' web page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # A page dedicated to a particular topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # A page for adding new topic
+    path('new_topic', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import TopicForm
 from .models import Topic
 
 
@@ -21,3 +22,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data sent; create a new empty form
+        form = TopicForm()
+    else:
+        # POST sent; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Show an empty or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
