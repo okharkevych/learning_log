6e039d38e626a4f25ca53a18e0b9ae37e011e5b0
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-06 13:43:32 +0300

    added user account registration functionality

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
     # Include default auth URLs
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        # Show an empty registration form
+        form = UserCreationForm()
+    else:
+        # Process the filled in form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Authorize user and redirect to homepage
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Show an empty or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
