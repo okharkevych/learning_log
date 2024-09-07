52e8b1a8a6e6655858c030936f15bebd5a185a04
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-07 13:49:59 +0300

    added 'django-bootstrap4' to project requirements for future website design management; updated INSTALLED_APPS in settings.py accordingly

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # Third-party apps
+    'bootstrap4',
+
     # Deafult django apps
     'django.contrib.admin',
     'django.contrib.auth',
