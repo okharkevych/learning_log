b170717c24fe558e40d2727bb8551228811215b2
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-09 17:16:07 +0300

    set DEBUG option based on environment variables

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +132,8 @@ LOGIN_URL = 'users:login'
 import django_heroku
 
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
